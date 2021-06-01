0c0036fbba3ec94d5c8e99c8c54c1ade8fe84b67
yhhCh405-CS <yehtet10@cloudsource.co.jp>
2021-06-01 13:24:53 +0630

    unfoucs keyboard

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/yehtethein/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yehtethein/Data/Flutter Projects/Cloudsource/PACKAGES/google_map_location_picker/example"
+export "FLUTTER_APPLICATION_PATH=/Users/yehtethein/Data/Flutter Projects/Dreamwork/PACKAGES/google_map_location_picker/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
