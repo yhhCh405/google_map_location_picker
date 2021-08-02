2a09af56b6adc91347d54597dfb2d703021661fd
yhhCh405-CS <yehtet10@cloudsource.co.jp>
2021-08-02 21:04:03 +0630

    version downgrade for legacy builds

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:geolocator/geolocator.dart';
+import 'package:geolocator/geolocator.dart' hide openAppSettings;
 
 import 'package:google_map_location_picker/src/providers/location_provider.dart';
 import 'package:google_map_location_picker/src/utils/loading_builder.dart';
